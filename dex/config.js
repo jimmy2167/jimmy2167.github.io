16a7926095e7d0746c40cef59a6c61ef6ef0a2c4
Bitsoley <87956155+jimmy2167@users.noreply.github.com>
2025-08-16 04:00:53 -0400

    Create config.js

---
+++ dex/config.js
@@ -0,0 +1,8 @@
+// site/config.js
+const CONFIG = {
+  TOKEN_ADDRESS:  "0xA0F67daB97bA700736A334bE95CcBae864D1E81a",
+  WBSO_ADDRESS:   "0x3De8aDe36A4749258DAb0f1C06dFF3ca99CB6618",
+  ROUTER_ADDRESS: "0x12f5cD3A3Fd1d399b20f6d6aBfac8E6485B5041d",
+  CHAIN_ID:       216721,
+  RPC_URL:        "https://rpc.bitsoley.com"
+};
