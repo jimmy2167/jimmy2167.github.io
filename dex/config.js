f56a4ec38523a7829c5d25d13b18438b0f288b59
Bitsoley <87956155+jimmy2167@users.noreply.github.com>
2025-08-16 03:59:16 -0400

    Delete dex/config.js

--- dex/config.js
+++
@@ -1,6 +0,0 @@
-// Copy this file to site/config.js and fill in your deployed addresses
-const CONFIG = {
-  TOKEN_ADDRESS: "0xA0F67daB97bA700736A334bE95CcBae864D1E81a",
-  WBSO_ADDRESS:  "0x3De8aDe36A4749258DAb0f1C06dFF3ca99CB6618",
-  ROUTER_ADDRESS:"0x12f5cD3A3Fd1d399b20f6d6aBfac8E6485B5041d"
-};
